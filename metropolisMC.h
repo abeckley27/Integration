3d1c1474f8055908d0ac5db4291b5bb28c272619
Aidan Beckley <aidanbeckley@gmail.com>
2020-01-31 18:34:32 -0600

    Consolidate mmc functions

--- metropolisMC.h
+++ metropolisMC.h
@@ -8,9 +8,16 @@
 #include <random>
 
 // Sampling functions
+// 1D
 double g(double x) { return exp(-1 * x * x ); }
 double f1(double x) { return x*x; }
 double f2(double x) { return cos(10*x*x); }
+
+// 2D
+double g(double x, double y) { return exp(-1 * x * x - y * y ) ; }
+double f1(double x, double y) { return x * x * y * y; }
+
+// 3D
 
 //Integrating functions
 
@@ -20,6 +27,7 @@ double mmc_1d() {
 	std::uniform_real_distribution<> acc_dist(0.0,1.0);
 	std::uniform_real_distribution<> sampling_dist(-1.0,1.0);
 	double pi = 3.14159265358979;
+	double result = 0.0;
 
 	for (int p = 2; p<=8; p += 2) {
 
@@ -43,12 +51,49 @@ double mmc_1d() {
 			i++;
 		}
  
-		double result = sqrt(pi) * tot / steps;
-		std::cout << "The integral is \t" << result << endl;
-		std::cout << "Acceptance ratio: \t" << acc / steps << endl;
-		std::cout << "Nsteps: \t\t" << steps << endl << "---\n";
+		result = sqrt(pi) * tot / steps;
+		std::cout << "The integral is \t" << result << std::endl;
+		std::cout << "Acceptance ratio: \t" << acc / steps << std::endl;
+		std::cout << "Nsteps: \t\t" << steps << std::endl << "---\n";
 	}
 	return result;
 }
 
+double mmc_2d() {
 
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_real_distribution<> acc_dist(0.0,1.0);
+	std::uniform_real_distribution<> sampling_dist(-1.0,1.0);
+	double pi = 3.14159265358979;
+
+	double x = sampling_dist(gen);
+	double y = sampling_dist(gen);
+	int i = 0;
+	int p = 7;
+	int steps = pow(10,p);
+	double tot = 0.0;
+	double acc = 0.0;
+	double step = 1.0;
+
+	while (i < steps) {
+		double x1 = x + step * sampling_dist(gen);
+		double y1 = y + step * sampling_dist(gen);
+		double u0 = g(x,y);
+		double u1 = g(x1,y1);
+		double acceptance_crit = acc_dist(gen);
+		if ( (u1 / u0) > acceptance_crit ) { 
+			x = x1;
+			y = y1;
+			acc++;
+		}
+		tot += f1(x,y);
+		i++;
+	}
+ 
+	std::cout << "The integral is \t" << pi * tot / steps << std::endl;
+	std::cout << "Acceptance ratio: \t" << acc / steps << std::endl;
+	std::cout << "Nsteps: \t\t" << steps << std::endl << "---\n";
+
+	return 0;
+}
